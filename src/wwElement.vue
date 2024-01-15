e32608514f4550b1d21586aef9431d037affc77f
wlucaswPC <lucas.panier@ext.bpifrance.fr>
2024-01-15 10:10:31 +0100

    add graph

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,22 +1,108 @@
 <template>
-  <div class="my-element">
-    <p :style="textStyle">I am a custom element !</p>
-  </div>
+  <BubbleGraph :bubblesProps="bubblesProps" />
 </template>
 
 <script>
+import BubbleGraph from "./components/BubbleGraph.vue";
+import { ref } from "vue";
+
 export default {
-  props: {
-    content: { type: Object, required: true },
-  },
-  computed: {
-    textStyle() {
-      return {
-        color: this.content.textColor,
-      };
-    },
+  name: "App",
+  components: {
+    BubbleGraph,
   },
 };
+</script>
+
+<script setup>
+const data = [
+  {
+    color: "#FEDACC",
+    scale: 100,
+    text: "SARL John Doe",
+    xPosition: 0,
+    yPosition: 300,
+  },
+  {
+    color: "#696969",
+    textColor: "white",
+    scale: 50,
+    text: "Italy",
+    xPosition: 300,
+    yPosition: 0,
+  },
+  {
+    color: "#FEDACC",
+    scale: 45,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 10,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 5,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 1,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 99,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 80,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 25,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 89,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 14,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+  {
+    color: "#FEDACC",
+    scale: 67,
+    text: "Ireland",
+    xPosition: 200,
+    yPosition: 200,
+  },
+];
+const bubblesProps = ref(data.sort((a, b) => b.scale - a.scale));
 </script>
 
 <style lang="scss" scoped>
